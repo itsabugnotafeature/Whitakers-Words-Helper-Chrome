49fd98dc81b786f550c2141effa4102512b7cc53
Max Sonderegger <maxmsonderegger@gmail.com>
2019-03-10 20:03:37 -0400

    Moved pop up structure to ShadowDOM

--- script.js
+++ script.js
@@ -1,56 +1,64 @@
 'use strict';
 
+var WWHHost = document.createElement("DIV");
+WWHHost.id = "WWH_host";
+WWHHost.style.position = "static";
+var shadowRoot = WWHHost.attachShadow({mode: 'closed'});
+shadowRoot.innerHTML = "<style>#WWH_pop_up_div {background-color: rgb(110, 110, 110);padding: 2px !important;border-radius: 10px !important;position: absolute !important;visibility: hidden;min-width: 450px;z-index: 100000;opacity: 1 !important;font-family: Consolas, monospace !important;}#WWH_internal_div {background-color: rgb(255, 255, 255);border-radius: 15px !important;padding-left: 5px !important;padding-right: 5px !important;}#WWH_pop_up_div pre {font-family: Consolas, monospace !important;font-size: 14px !important;padding: 0 !important;margin: 0 !important;background-color: rgb(255, 255, 255, 0) !important;color: rgb(0, 0, 0) !important;box-shadow: none !important;line-height: 100% !important;}</style>"
 var pop_up_div = document.createElement("DIV");
 pop_up_div.id = "WWH_pop_up_div";
 var internal_div = document.createElement("DIV");
 internal_div.id = "WWH_internal_div"
-var pre_text = document.createElement("PRE");
-internal_div.appendChild(pre_text);
+var internal_pre = document.createElement("PRE");
+internal_div.appendChild(internal_pre);
 pop_up_div.appendChild(internal_div);
                           
-document.body.append(pop_up_div);
+shadowRoot.append(pop_up_div);
+
+document.body.after(WWHHost);
 
 document.addEventListener("click", function(event) {
-                          document.getElementById("WWH_pop_up_div").style.visibility = 'hidden';
+                          pop_up_div.style.visibility = 'hidden';
 });
 
 document.addEventListener("dblclick", function(event) {
     if (window.getSelection() && document.activeElement.tagName.toLowerCase() != "input" && document.activeElement.tagName.toLowerCase() != "textarea") {
         var text = window.getSelection().toString();
         if (text != "") {
-            $( "#WWH_pop_up_div" ).css({'visibility' : 'visible'});
-            $( "#WWH_pop_up_div" ).offset({left:event.pageX,top:event.pageY});
-            $( "#WWH_pop_up_div #WWH_internal_div pre" ).text("");
-                          chrome.runtime.sendMessage({query: text}, function(response) {
-                                                     $( "#WWH_pop_up_div #WWH_internal_div pre" ).text(response.responseText);
-                                                     var pop_up_div = document.getElementById("WWH_pop_up_div");
-                                                     var div_rect = pop_up_div.getBoundingClientRect();
-                                                     if (div_rect.x + div_rect.width > document.documentElement.clientWidth) {
-                                                     pop_up_div.style.left = (document.documentElement.clientWidth - div_rect.width - 5).toString() + "px";
-                                                     }
-                                                     if (div_rect.y + div_rect.height > document.documentElement.clientHeight) {
-                                                     pop_up_div.style.top = (document.documentElement.clientHeight - div_rect.height + window.scrollY - 5).toString() + "px";
-                                                     }
-                                                     });
+            pop_up_div.style.visibility = 'visible';
+            pop_up_div.style.left = event.pageX.toString() + "px";
+            pop_up_div.style.top = event.pageY.toString() + "px";
+            internal_pre.textContent = "";
+            chrome.runtime.sendMessage({query: text}, function(response) {
+                internal_pre.textContent = response.responseText;
+                var div_rect = pop_up_div.getBoundingClientRect();
+                if (div_rect.x + div_rect.width > document.documentElement.clientWidth) {
+                    pop_up_div.style.left = (document.documentElement.clientWidth - div_rect.width - 5).toString() + "px";
+                }
+                if (div_rect.y + div_rect.height > document.documentElement.clientHeight) {
+                    pop_up_div.style.top = (document.documentElement.clientHeight - div_rect.height + window.scrollY - 5).toString() + "px";
+                }
+                console.log("Left: " + pop_up_div.style.left + "; Top: " + pop_up_div.style.top);
+            });
         }
     }
 });
 
 document.addEventListener("contextmenu", function(event) {
-                            $( "#WWH_pop_up_div" ).offset({left:event.pageX,top:event.pageY});
-                            $( "#WWH_pop_up_div" ).css({'visibility' : 'hidden'});
-                          })
+    pop_up_div.style.visibility = 'hidden';
+    pop_up_div.style.left = event.pageX.toString() + "px";
+    pop_up_div.style.top = event.pageY.toString() + "px";
+});
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-                                         $( "#WWH_pop_up_div" ).css({'visibility' : 'visible'});
-                                         $( "#WWH_pop_up_div #WWH_internal_div pre" ).text(request.responseText);
-                                         var pop_up_div = document.getElementById("WWH_pop_up_div");
-                                         var div_rect = pop_up_div.getBoundingClientRect();
-                                         if (div_rect.x + div_rect.width > document.documentElement.clientWidth) {
-                                            pop_up_div.style.left = (document.documentElement.clientWidth - div_rect.width - 5).toString() + "px";
-                                         }
-                                         if (div_rect.y + div_rect.height > document.documentElement.clientHeight) {
-                                            pop_up_div.style.top = (document.documentElement.clientHeight - div_rect.height + window.scrollY - 5).toString() + "px";
-                                         }
-                                     });
+    pop_up_div.style.visibility = 'visible';
+    internal_pre.textContent = request.responseText;
+    var div_rect = pop_up_div.getBoundingClientRect();
+    if (div_rect.x + div_rect.width > document.documentElement.clientWidth) {
+        pop_up_div.style.left = (document.documentElement.clientWidth - div_rect.width - 5).toString() + "px";
+    }
+    if (div_rect.y + div_rect.height > document.documentElement.clientHeight) {
+        pop_up_div.style.top = (document.documentElement.clientHeight - div_rect.height + window.scrollY - 5).toString() + "px";
+    }
+});
 
